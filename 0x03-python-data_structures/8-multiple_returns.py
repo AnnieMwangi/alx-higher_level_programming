c9e23a110e99d8736638f8f5da692a341fc2fe6f
AnnieMwangi <anne.mwangechi@gmail.com>
2022-06-07 06:03:39 +0300

    Create 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    len_sen = len(sentence)
+
+    if (len_sen == 0):
+        new_tuple = (len_sen, None)
+    else:
+        new_tuple = (len_sen, sentence[0])
+
+    return (new_tuple)
+© 2022 GitHub, Inc.
+Terms
